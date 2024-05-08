e4390ecc0b5bf6666fe11ddf7ee0777398932c85
DanarchyLarusson <larussondaniel@gmail.com>
2024-05-08 14:11:59 +0200

    Added logic for member handling

---
+++ BokLog/Controller/MemberController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using BokLogg.Model;
+
+namespace BokLogg.Controller
+{
+    public class MemberController
+    {
+        private List<Member> members;
+        private string relativePathMembers = Path.Combine(Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..", "Data")), "people.json");
+
+        public MemberController()
+        {
+            members = LoadMembersFromJson();
+        }
+
+        private List<Member> LoadMembersFromJson()
+        {
+            try
+            {
+                string jsonData = File.ReadAllText(relativePathMembers);
+                return JsonSerializer.Deserialize<List<Member>>(jsonData);
+            }
+            catch (Exception)
+            {
+                // Handle exceptions
+                return new List<Member>();
+            }
+        }
+
+        private void SaveMembersToJson(List<Member> members)
+        {
+            try
+            {
+                string jsonData = JsonSerializer.Serialize(members, typeof(List<Member>), new JsonSerializerOptions
+                {
+                    WriteIndented = true
+                });
+                File.WriteAllText(relativePathMembers, jsonData);
+            }
+            catch
+            {
+                // Handle exceptions
+            }
+        }
+
+        public void AddMember(Member member)
+        {
+            members.Add(member);
+            SaveMembersToJson(members);
+        }
+
+        public List<Member> GetAllMembers()
+        {
+            return members;
+        }
+
+        public Member GetMemberByName(string firstName, string lastName)
+        {
+            return members.FirstOrDefault(m => m.FirstName == firstName && m.LastName == lastName);
+        }
+
+        public void CorrectOverdueBooks(string firstName, string lastName, int newOverdueBooks)
+        {
+            Member member = GetMemberByName(firstName, lastName);
+            if (member != null)
+            {
+                member.NumberOfOverdueBooks = newOverdueBooks;
+                List<Member> updatedMembers = new List<Member>(members);
+                SaveMembersToJson(updatedMembers);
+            }
+            else
+            {
+                // Handle member not found
+            }
+        }
+
+        public void CorrectFailedReturns(string firstName, string lastName, int newFailedReturns)
+        {
+            Member member = GetMemberByName(firstName, lastName);
+            if (member != null)
+            {
+                member.NumberOfFailedReturns = newFailedReturns;
+                List<Member> updatedMembers = new List<Member>(members);
+                SaveMembersToJson(updatedMembers);
+            }
+            else
+            {
+                // Handle member not found
+            }
+        }
+    }
+}
+
+
--- BokLog/Model/Member.cs
+++ BokLog/Model/Member.cs
@@ -11,6 +11,23 @@ namespace BokLogg.Model
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public DateTime DateOfRegistry { get; set; } = DateTime.Now;
-        
+        public int NumberOfLoans { get; set; }
+        public int NumberOfOverdueBooks { get; set; }
+        public int NumberOfFailedReturns { get; set; }
+
+        public override string ToString()
+        {
+            // Create a string representation of the member
+            string memberInfo = $"--------------------------------\n" +
+                                $"Namn: {FirstName} {LastName}\n" +
+                                $"Registrerad: {DateOfRegistry.ToString("d")}\n" +
+                                $"Antal Lån: {NumberOfLoans}\n" +
+                                $"Försenade Lån: {NumberOfOverdueBooks}\n" +
+                                $"Ej Återlämnande lån: {NumberOfFailedReturns}\n" +
+                                $"--------------------------------\n";
+
+            return memberInfo;
+        }
     }
+
 }
